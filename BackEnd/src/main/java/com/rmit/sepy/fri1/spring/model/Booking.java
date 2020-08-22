9468867dbff759e396258ec81938dc776e4e41c5
Jonathon Mitterbacher <s3784464@student.rmit.edu.au>
2020-08-22 14:59:07 +1000

    created booking class, updated application.properties

---
+++ BackEnd/src/main/java/com/rmit/sepy/fri1/spring/model/Booking.java
@@ -0,0 +1,54 @@
+package com.rmit.sepy.fri1.spring.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Date;
+import java.sql.Time;
+
+@Entity
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    /* bookingID formula
+       e.g. "2208202"
+       first 3 letters of bookingName i.e. scuba -> scu
+       date as integers i.e. 22/08/2020 -> 220820
+       add booking count i.e. 3rd booking add 2 to id
+     */
+    private long bookingId;
+    private String bookingName;
+    private String workerName;
+    private String location;
+
+    private Date date;
+    private Time startTime;
+    private Time endTime;
+
+    public Booking(){
+    }
+
+    public Long getId() {return bookingId; }
+    public void setId(Long bookingId) { this.bookingId=bookingId; }
+
+    public String getName() {return bookingName; }
+    public void setId(String bookingName) { this.bookingName=bookingName; }
+
+    public String getWorkerName() {return workerName; }
+    public void setWorkerName(String workerName) { this.workerName=workerName; }
+
+    public String getLocation() {return location; }
+    public void setlocation(String location) { this.location=location; }
+
+    public Date getDate() {return date; }
+    public void setDate(Date date) { this.date=date; }
+
+    public Time getStartTime() {return startTime; }
+    public void setStartTime(Time startTime) { this.startTime=startTime; }
+
+    public Time getEndTime() {return endTime; }
+    public void setEndTime(Time endTime) { this.endTime=endTime; }
+
+}
