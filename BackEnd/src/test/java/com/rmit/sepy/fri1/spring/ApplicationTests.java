4f62ce18425b20d597d9733c8036ffac3b22d237
Jonathon Mitterbacher <s3784464@student.rmit.edu.au>
2020-08-22 13:53:03 +1000

    Added Springboot to backend

---
+++ BackEnd/src/test/java/com/rmit/sepy/fri1/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmit.sepy.fri1.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
