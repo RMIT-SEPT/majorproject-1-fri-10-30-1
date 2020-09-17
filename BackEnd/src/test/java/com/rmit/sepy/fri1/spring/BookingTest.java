db3ddea08e9542e48365e72d8a0405bf28145259
Jamaine George <s3660962@student.rmit.edu.au>
2020-09-17 22:55:05 +1000

    Unit tests for bookingName and workerName

---
+++ BackEnd/src/test/java/com/rmit/sepy/fri1/spring/BookingTest.java
@@ -0,0 +1,33 @@
+import com.rmit.sepy.fri1.spring.model.Booking;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookingTest {
+
+    @Test
+    void getBookingName() {
+        Booking driving = new Booking();
+        assertEquals("Driving", driving.getBookingName());
+    }
+
+    @Test
+    void setBookingName() {
+        Booking driving = new Booking();
+        driving.setBookingName("Driving");
+        assertEquals("Driving", driving.getBookingName());
+    }
+
+    @Test
+    void getWorkerName() {
+        Booking jamaine = new Booking();
+        assertEquals("Jamaine", jamaine.getWorkerName());
+    }
+
+    @Test
+    void setWorkerName() {
+        Booking jamaine = new Booking();
+        jamaine.setWorkerName("Jamaine");
+        assertEquals("Jamaine", jamaine.getWorkerName());
+    }
+}
