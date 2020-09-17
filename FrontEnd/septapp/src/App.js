3380dae7346ff47346e25dfb2a028f5779dcbe27
Jonathon Mitterbacher <s3784464@student.rmit.edu.au>
2020-09-17 16:29:23 +1000

    cleaned up old files, added Footer.js

--- FrontEnd/septapp/src/App.js
+++ FrontEnd/septapp/src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import './style.css';
+import './App.css';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 
 import {Provider} from "react-redux";
 import store from './store';
 
 import Header from './components/Layout/Header';
+import Footer from './components/Layout/Footer';
 import Dashboard from './components/Dashboard';
 import AddPerson from './components/Persons/AddPerson';
-import Booking from './components/Booking';
 import Welcome from './components/Welcome';
-import ByPerson from "./components/Bookings/ByPerson";
-import ByService from "./components/Bookings/ByService";
 import Services from "./components/Bookings/Services";
 import People from "./components/Bookings/People";
 import Availability from "./components/Bookings/Availability";
@@ -28,16 +26,12 @@ function App() {
         <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/addPerson" component={AddPerson} />
 
-        {/*Old booking process*/}
-        {/*<Route exact path="/booking" component={Booking} />*/}
-        {/*<Route exact path="/byPerson" component={ByPerson} />*/}
-        {/*<Route exact path="/byService" component={ByService} />*/}
-
-        {/*New Booking process*/}
+        {/*Booking process*/}
         <Route exact path="/booking" component={Services} />
         <Route exact path="/people" component={People} />
         <Route exact path="/availability" component={Availability} />
     </div>
+    <Footer/>
     </Router>
     </Provider>
   );
--- FrontEnd/septapp/src/components/Layout/Header.js
+++ FrontEnd/septapp/src/components/Layout/Header.js
@@ -29,15 +29,16 @@ import React, { Component } from 'react'
 
                             <ul className="navbar-nav ml-auto">
                                 <li className="nav-item">
-                                    <a className="nav-link " href="/register">
-                                        Register
-                                    </a>
-                                </li>
-                                <li className="nav-item">
                                     <a className="nav-link" href="/login">
                                         Login
                                     </a>
                                 </li>
+                                <li className="nav-item">
+                                    <a className="nav-link " href="/register">
+                                        Register
+                                    </a>
+                                </li>
+                                
                             </ul>
 
                         </div>
