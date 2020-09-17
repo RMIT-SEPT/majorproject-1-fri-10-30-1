9357d0cb67470aae532269da997970eceee0a2f1
Jamainne <s3660962@student.rmit.edu.au>
2020-09-18 00:42:32 +1000

    Unit Test fix

---
+++ BackEnd/src/test/BookingTest.java
@@ -0,0 +1,32 @@
+import com.rmit.sepy.fri1.spring.model.Booking;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+class BookingTest {
+
+    @Test
+    void getBookingName() {
+        Booking driving = new Booking();
+        assertEquals("Driving", driving.getBookingName());
+    }
+
+    @Test
+    void setBookingName() {
+        Booking driving = new Booking();
+        driving.setBookingName("Driving");
+        assertEquals("Driving", driving.getBookingName());
+    }
+
+    @Test
+    void getWorkerName() {
+        Booking jamaine = new Booking();
+        assertEquals("Jamaine", jamaine.getWorkerName());
+    }
+
+    @Test
+    void setWorkerName() {
+        Booking jamaine = new Booking();
+        jamaine.setWorkerName("Jamaine");
+        assertEquals("Jamaine", jamaine.getWorkerName());
+    }
+}
