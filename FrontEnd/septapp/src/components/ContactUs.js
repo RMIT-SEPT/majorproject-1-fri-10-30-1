13f29c312c50f1d8db99a48b75a258dc99922efc
Jonathan <s3722092@users.noreply.github.com>
2020-09-22 23:46:44 +1000

    Contact form implemented

--- FrontEnd/septapp/src/components/ContactUs.js
+++ FrontEnd/septapp/src/components/ContactUs.js
@@ -4,7 +4,23 @@ class ContactUs extends Component {
     render() {
         return (
             <div className="ContactUs">
-                //put all html code here inside the two div tags
+                <h1>Get In Touch</h1>
+                <article>
+
+                    <p>Phone: 93327348</p>
+                    <p>Address: 123 ABC Street</p>
+                </article>
+                <div className="container">
+                    <form method='post' action="mailto:admin@sept.com.au"/>
+                    <label>Full Name </label>
+                    <input type="text" id="firstname" required/>
+                    <label>Email</label>
+                    <input type="email" textmode="email" id="email" pattern="[a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,}" required/>
+                    <label>Message</label>
+                    <textarea type="text" id="message" required placeholder="Write something.."/>
+                    <input type="submit" value="Submit"/>
+
+                </div>
             </div>
 
         )
