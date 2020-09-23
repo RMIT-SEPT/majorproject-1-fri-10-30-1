e1b7448dcd4ab8decb7fa1b525d02d5260eaf96e
Jamainne <s3660962@student.rmit.edu.au>
2020-09-23 12:49:06 +1000

    api fix

--- FrontEnd/septapp/src/BookingService.js
+++ FrontEnd/septapp/src/BookingService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BOOKING_API_BASE_URL = "http://localhost:8080/api/booking";
+const BOOKING_API_BASE_URL = "http://localhost:8080/api/booking/booking";
 
 class BookingService {
 
