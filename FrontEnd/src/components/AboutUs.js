c72ee294a17275a9834620328d1d27bbfa61797f
Jamainne <s3660962@student.rmit.edu.au>
2020-10-15 00:53:45 +1100

    About Us Content

---
+++ FrontEnd/src/components/AboutUs.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+class AboutUs extends Component {
+    render() {
+        return (
+            <div className="AboutUs">
+                <h1>About AGME</h1>
+                <article>
+                <br></br>
+                    <h1>Frequently Asked Questions</h1>
+                    <br></br>
+                    <h2>Q1. What is AGME?</h2>
+                    <p>AGME is a system to provide online booking services for their clients</p>
+                    <br></br>
+                    <h2>Q2. What is AGME?</h2>
+                    <p>AGME is a system to provide online booking services for their clients</p>
+                    <br></br>
+                    <h2>Q3. What is AGME?</h2>
+                    <p>AGME is a system to provide online booking services for their clients</p>
+                    <br></br>
+                    <h2>Q4. What is AGME?</h2>
+                    <p>AGME is a system to provide online booking services for their clients</p>
+                    <br></br>
+                </article>
+            </div>
+
+        )
+    }
+}
+export default AboutUs;
