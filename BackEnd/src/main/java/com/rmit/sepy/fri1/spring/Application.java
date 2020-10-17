833600776cb3b4818ce407f14861da94c2ee441e
Jamainne <s3660962@student.rmit.edu.au>
2020-10-17 13:25:55 +1100

    Comments in Application.java

--- BackEnd/src/main/java/com/rmit/sepy/fri1/spring/Application.java
+++ BackEnd/src/main/java/com/rmit/sepy/fri1/spring/Application.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
+//Defines spring framwork
 @SpringBootApplication
 public class Application {
-
+    //Application start
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -17,6 +17,7 @@ public class Application {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
             @Override
+            //Bypasses CORS error when creating bookings
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/booking").allowedOrigins("http://localhost:3000");
             }
